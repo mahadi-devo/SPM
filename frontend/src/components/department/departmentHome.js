d29c11526acc04f54be6086f8881cc85137a5c41
Avishka Shyaman <avishkashyaman@gmail.com>
2021-08-15 21:59:51 +0530

    Added DepartmentHome component

---
+++ frontend/src/components/department/departmentHome.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+  Container,
+  Box,
+  Spacer,
+  VStack,
+  HStack,
+  Heading,
+  Text,
+  Button,
+} from '@chakra-ui/react';
+import { FaPlus, FaEye, FaEdit, FaTrashAlt} from 'react-icons/fa';
+
+import CustomTable from '../shared/customTable';
+
+const DepartmentHome = (props) => {
+  const Handelview = (id) => {
+    console.log('clicked view on ', id);
+  }
+  
+  const HandelEdit = (id) => {
+    console.log('clicked edit on ', id);
+  }
+  
+  const HandelDelete = (id) => {
+    console.log('clicked delete on ', id);
+  }
+
+  const cols = [
+    {
+      title: 'Department ID',
+      render: (data) => {
+        return data.id;
+      },
+    },
+    {
+      title: 'Department Name',
+      render: (data) => {
+        return data.name;
+      },
+    },
+    {
+      title: 'Manager Name',
+      // isNumeric: true,
+      render: (data) => {
+        return data.manager;
+      },
+    },
+    {
+      title: 'Manager Name',
+      // isNumeric: true,
+      render: (data) => {
+        return <HStack w="50%">
+          <FaEye cursor='pointer' onClick={() => Handelview(data.id)} color="blue" />
+          <Spacer />
+          <FaEdit cursor='pointer' onClick={() => HandelEdit(data.id)} color="yellow" />
+          <Spacer />
+          <FaTrashAlt cursor='pointer' onClick={() => HandelDelete(data.id)} color="red" />
+        </HStack>;
+      },
+    },
+  ];
+
+  const rows = [
+    {
+      id: 'IT',
+      name: 'Information Technology ',
+      manager: 'Nuwan Chamara',
+    },
+    {
+      id: 'ET',
+      name: 'Information Technology ',
+      manager: 'Nuwan Chamara',
+    },
+    {
+      id: 'BT',
+      name: 'Information Technology ',
+      manager: 'Nuwan Chamara',
+    },
+    {
+      id: 'ENG',
+      name: 'Information Technology ',
+      manager: 'Nuwan Chamara',
+    },
+    {
+      id: 'BA',
+      name: 'Information Technology ',
+      manager: 'Nuwan Chamara',
+    },
+  ];
+
+  return (
+    <Container maxW="100%" centerContent={true}>
+      <VStack w="90%" alignItems="stretch" mt={5}>
+        <HStack>
+          <Heading as="h1" size="xl">
+            Department Management
+          </Heading>
+          <Spacer />
+          <Button leftIcon={<FaPlus />} colorScheme="blue" variant="solid">
+            ADD
+          </Button>
+        </HStack>
+        <CustomTable cols={cols} rows={rows}/>
+      </VStack>
+    </Container>
+  );
+};
+
+export default DepartmentHome;
---
+++ frontend/src/components/department/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DepartmentHome from './departmentHome'
+
+const Department = () => {
+  return (
+    <div>
+      <DepartmentHome />
+    </div>
+  )
+}
+
+export default Department;
