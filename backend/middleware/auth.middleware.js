99d9e599d9105609da82af25cd206d7451934f33
pasinduj97 <pasinduj9@gmail.com>
2021-08-23 21:38:23 +0530

    hi

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -5,11 +5,8 @@ exports.authorize = async (req, res, next) => {
   try {
     let token;
 
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith('Bearer ')
-    ) {
-      token = req.headers.authorization.split(' ')[1];
+    if (req.headers.authorization) {
+      token = req.headers.authorization;
     }
 
     if (!token) {
