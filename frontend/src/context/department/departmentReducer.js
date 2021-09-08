e7890caf31d1177bcb5002d981a2b772bbc18c54
Avishka Shyaman <avishkashyaman@gmail.com>
2021-09-08 22:06:12 +0530

    Modified add Department context API functions in frontend

--- frontend/src/context/department/departmentReducer.js
+++ frontend/src/context/department/departmentReducer.js
@@ -3,7 +3,6 @@ import { ADD_DEPARTMENT, GET_DEPARTMENTS } from './actions';
 const CustomerReducer = (state, action) => {
   switch (action.type) {
     case ADD_DEPARTMENT:
-      state.depatments.push(action.payload);
       return {
         ...state,
         depatments: [...state.depatments],
