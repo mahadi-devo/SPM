97eaf62021ac59170be738d065fee2e2ae3a6471
mahadi-devo <mahadihassan083@gmail.com>
2021-10-01 21:34:51 +0530

    chat crud added

---
+++ backend/controllers/chat.controller.js
@@ -0,0 +1,104 @@
+const Chat = require('../models/chat.model');
+const Ticket = require('../models/ticket.model');
+const ApiError = require('../utils/apiError');
+
+const getChatByTicketId = async (req, res, next) => {
+  try {
+    const chat = await Chat.find({ ticketId: req.params.id })
+      .populate('user')
+      .sort({ createdAt: -1 });
+    return res.status(200).json({
+      success: true,
+      data: chat,
+    });
+  } catch (e) {
+    if (e.status) {
+      console.log(e);
+    }
+    return next(new ApiError(e.message, 500));
+  }
+};
+
+const createChatByTicketId = async (req, res, next) => {
+  try {
+    const { message } = req.body;
+    console.log(message);
+
+    const ticket = await Ticket.findOne({ id: req.params.id });
+
+    if (!ticket) {
+      return new ApiError('Ticket is not exist', 400);
+    }
+
+    const chat = await Chat.create({
+      user: req.user._id,
+      ticketId: req.params.ticketId,
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      chat,
+    });
+  } catch (e) {
+    if (e.status) {
+      console.log(e);
+    }
+    return next(new ApiError(e.message, 500));
+  }
+};
+
+const updateChatByChatId = async (req, res, next) => {
+  try {
+    const { message } = req.body;
+
+    const chat = await Chat.findById(req.params.id);
+
+    if (!chat) {
+      return new ApiError('Chat not exist', 400);
+    }
+
+    const chatResponse = await Chat.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          message,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: chatResponse,
+    });
+  } catch (e) {
+    if (e.status) {
+      console.log(e);
+    }
+    return next(new ApiError(e.message, 500));
+  }
+};
+
+const deleteChatById = async (req, res, next) => {
+  try {
+    const chat = await Chat.findByIdAndRemove(req.params.id);
+
+    res.status(200).json({
+      chat,
+      success: true,
+    });
+  } catch (e) {
+    if (e.status) {
+      console.log(e);
+    }
+    return next(new ApiError(e.message, 500));
+  }
+};
+
+module.exports = {
+  getChatByTicketId,
+  createChatByTicketId,
+  updateChatByChatId,
+  deleteChatById,
+};
--- frontend/src/components/admin/AdminDashboard/index.js
+++ frontend/src/components/admin/AdminDashboard/index.js
@@ -13,72 +13,83 @@ import Ticket from '../ticket/ticket';
 import User from '../user/UserRoute';
 import Topbar from '../../shared/topbar';
 import Department from '../../department';
-import ViewTicket from '../../customer/CustomerDashboard/ViewTicket';
 import EditTicket from '../ticket/editTicket';
 import Login from '../../auth/Login';
+import CustomerState from '../../../context/customer/CustomerState';
 
 const AdminDashboard = () => {
   const { path } = useRouteMatch();
   return (
     <Router>
-      <Box minH='100vh' bg='#rgba(255, 255, 255, 0.80)'>
-        <Switch>
-          <Route exact path='/login' component={Login} />
-        </Switch>
-        <Sidebar />
-
-        <Topbar
-          entitlement={[AccessRights.admin, AccessRights.organizationalUser]}
-        />
-
-        <Box ml={{ base: 0, md: 60 }} p='4'>
+      <CustomerState>
+        <Box minH='100vh' bg='#rgba(255, 255, 255, 0.80)'>
           <Switch>
-            <PrivateRoute
-              exact
-              path={`${path}`}
-              component={Ticket}
-              entitlement={[
-                AccessRights.admin,
-                AccessRights.organizationalUser,
-              ]}
-            />
-            <PrivateRoute
-              exact
-              path={`${path}/tickets`}
-              component={Ticket}
-              entitlement={[
-                AccessRights.admin,
-                AccessRights.organizationalUser,
-              ]}
-            />
-            <PrivateRoute
-              exact
-              path={`${path}/tickets/edit`}
-              component={EditTicket}
-              entitlement={[
-                AccessRights.admin,
-                AccessRights.organizationalUser,
-              ]}
-            />
-            <PrivateRoute
-              path={`${path}/users`}
-              component={User}
-              entitlement={[
-                AccessRights.admin,
-                AccessRights.organizationalUser,
-              ]}
-            />
-            <PrivateRoute
-              path={`${path}/departments`}
-              component={Department}
-              entitlement={[
-                AccessRights.admin,
-                AccessRights.organizationalUser,
-              ]}
-            />
+            <Route exact path='/login' component={Login} />
           </Switch>
+          <Sidebar />
+
+          <Topbar
+            entitlement={[AccessRights.admin, AccessRights.organizationalUser]}
+          />
+
+          <Box ml={{ base: 0, md: 60 }} p='4'>
+            <Switch>
+              <PrivateRoute
+                exact
+                path={`${path}`}
+                component={Ticket}
+                entitlement={[
+                  AccessRights.admin,
+                  AccessRights.organizationalUser,
+                ]}
+              />
+              <PrivateRoute
+                exact
+                path={`${path}/edit`}
+                component={EditTicket}
+                entitlement={[
+                  AccessRights.admin,
+                  AccessRights.organizationalUser,
+                ]}
+              />
+              <PrivateRoute
+                exact
+                path={`${path}/tickets`}
+                component={Ticket}
+                entitlement={[
+                  AccessRights.admin,
+                  AccessRights.organizationalUser,
+                ]}
+              />
+              <PrivateRoute
+                exact
+                path={`${path}/tickets/edit`}
+                component={EditTicket}
+                entitlement={[
+                  AccessRights.admin,
+                  AccessRights.organizationalUser,
+                ]}
+              />
+              <PrivateRoute
+                path={`${path}/users`}
+                component={User}
+                entitlement={[
+                  AccessRights.admin,
+                  AccessRights.organizationalUser,
+                ]}
+              />
+              <PrivateRoute
+                path={`${path}/departments`}
+                component={Department}
+                entitlement={[
+                  AccessRights.admin,
+                  AccessRights.organizationalUser,
+                ]}
+              />
+            </Switch>
+          </Box>
         </Box>
-      </Box>
+      </CustomerState>
     </Router>
   );
 };
